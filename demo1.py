f0e12d749d445af1d57b2c7d921f7e9f2f2253c9
Ngorruot <ngor.ruot@edu.sait.ca>
2025-10-16 14:54:49 -0600

    GPA

--- demo1.py
+++ demo1.py
@@ -1,3 +1,38 @@
+print('welcome to grading program ')
+
+student = {} # a dictionary made to store students name and avr GPA
+
+while True:
+    answer = input('Do you want to add a student(yes/no):')
+    if answer == "yes":
+        name = input("enter students name:")
+        gpas = [] #Created an empty list to store gpa's
+
+        print("Enter GPA values for the student's courses. Type -1 to stop.")
+        while True:
+            gpa_input = input('Enter GPAs: ')
+            if gpa_input == '-1':
+                break
+            gpa = float(gpa_input)
+            gpas.append(gpa)#adding gpa's to the current list of gpas 
+            
+        if gpas: 
+                average = sum(gpas) / len(gpas) #the sum/the number of gpa
+        else:
+                average = 0.0
+
+        student[name] = average 
+
+
+    elif answer == "no":
+        print(f'{name}')
+        for name, gpa in student.items():
+            print(name, ":", round(gpa, 2))
+        print("\nProgram ended.")
+        break
+    
+    else:
+         print('invalid input')
 
 
 
